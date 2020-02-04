ce9fca49ba0743a3ff22b743c3e73d244543cc8d
Kawsar Kamal <kawsar@hashicorp.com>
2020-02-04 16:28:49 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s Meowlicious app. Can I get a Wha Wha!
+  Welcome to ${PREFIX}'s Meowlicious app. 
   <!-- END -->
 
   </div>
