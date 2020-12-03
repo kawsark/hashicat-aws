844b55b0a42a5659621aa86033b2c3bddcd07905
kawsark <kawsar@hashicorp.com>
2020-12-02 23:34:19 -0500

    updated to 0.13

--- main.tf
+++ main.tf
@@ -124,6 +124,8 @@ resource aws_instance "hashicat" {
     Name = "${var.prefix}-hashicat-instance"
     owner = var.owner
     TTL = var.TTL
+    Department = "SE"
+    Billable = "yes"
   }
 }
 
---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
+  required_version = ">= 0.13"
+}
